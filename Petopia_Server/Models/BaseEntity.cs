3143c9d7f210c8d0d8aa2e93c31f64a10be0112c
AlexanderFRT <alexyetz@gmail.com>
2024-07-12 01:49:47 -0600

    Agregados los archivos iniciales del Back, terminados los modelados por completo y la configuración inicial del DB (Key + ApiDbContext), falta definir las relaciones para EF, la herramienta que le hara hashing a las contraseñas y agregar una funcion para que el DateAdded solo se actualice la primera vez

---
+++ Petopia_Server/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Petopia_Server.Models;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime DateAdded { get; set; }
+    public DateTime DateUpdated { get; set; }
+}
---
+++ Petopia_Server/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Petopia_Server.Models;
+
+public class User(string username, string fullName, string password, string email) : BaseEntity
+{
+    public string Username { get; set; } = username;
+    public string FullName { get; set; } = fullName;
+    public string Password { get; set; } = password;
+    public string Email { get; set; } = email;
+    public int? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+
+    public ICollection<Mascot> Mascots { get; set; } = [];
+}
