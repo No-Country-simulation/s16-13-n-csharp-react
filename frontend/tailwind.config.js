f35db464511a7c57eb0eb8153ffca1c68ec39d97
k-alex-leon <kelemon824@gmail.com>
2024-07-17 20:46:07 -0500

    feat: default colors pallete

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgb(var(--color-bg) / <alpha-value>)",
+        text: "rgb(var(--color-text) / <alpha-value>)",
+
+        primary: {
+          DEFAULT: "rgb(var(--color-primary) / <alpha-value>)",
+          light: {
+            DEFAULT: "rgb(var(--color-primary-light) / <alpha-value>)",
+            hover: "rgb(var(--color-primary-light-hover) / <alpha-value>)",
+            active: "rgb(var(--color-primary-light-active) / <alpha-value>)",
+          },
+          normal: {
+            DEFAULT: "rgb(var(--color-primary-normal) / <alpha-value>)",
+            hover: "rgb(var(--color-primary-normal-hover) / <alpha-value>)",
+            active: "rgb(var(--color-primary-normal-active) / <alpha-value>)",
+          },
+          dark: {
+            DEFAULT: "rgb(var(--color-primary-dark) / <alpha-value>)",
+            hover: "rgb(var(--color-primary-dark-hover) / <alpha-value>)",
+            active: "rgb(var(--color-primary-dark-active) / <alpha-value>)",
+          },
+          darker: "rgb(var(--color-primary-darker) / <alpha-value>)",
+        },
+
+        secondary: {
+          DEFAULT: "rgb(var(--color-secondary) / <alpha-value>)",
+          light: {
+            DEFAULT: "rgb(var(--color-secondary-light) / <alpha-value>)",
+            hover: "rgb(var(--color-secondary-light-hover) / <alpha-value>)",
+            active: "rgb(var(--color-secondary-light-active) / <alpha-value>)",
+          },
+          normal: {
+            DEFAULT: "rgb(var(--color-secondary-normal) / <alpha-value>)",
+            hover: "rgb(var(--color-secondary-normal-hover) / <alpha-value>)",
+            active: "rgb(var(--color-secondary-normal-active) / <alpha-value>)",
+          },
+          dark: {
+            DEFAULT: "rgb(var(--color-secondary-dark) / <alpha-value>)",
+            hover: "rgb(var(--color-secondary-dark-hover) / <alpha-value>)",
+            active: "rgb(var(--color-secondary-dark-active) / <alpha-value>)",
+          },
+          darker: "rgb(var(--color-secondary-darker) / <alpha-value>)",
+        },
+
+        accent: {
+          DEFAULT: "rgb(var(--color-accent) / <alpha-value>)",
+          light: {
+            DEFAULT: "rgb(var(--color-accent-light) / <alpha-value>)",
+            hover: "rgb(var(--color-accent-light-hover) / <alpha-value>)",
+            active: "rgb(var(--color-accent-light-active) / <alpha-value>)",
+          },
+          normal: {
+            DEFAULT: "rgb(var(--color-accent-normal) / <alpha-value>)",
+            hover: "rgb(var(--color-accent-normal-hover) / <alpha-value>)",
+            active: "rgb(var(--color-accent-normal-active) / <alpha-value>)",
+          },
+          dark: {
+            DEFAULT: "rgb(var(--color-accent-dark) / <alpha-value>)",
+            hover: "rgb(var(--color-accent-dark-hover) / <alpha-value>)",
+            active: "rgb(var(--color-accent-dark-active) / <alpha-value>)",
+          },
+          darker: "rgb(var(--color-accent-darker) / <alpha-value>)",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
