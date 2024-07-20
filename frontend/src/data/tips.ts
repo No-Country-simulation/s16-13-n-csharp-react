ff5f4b7cbd141c73df97fdb4289fb17cb9ddc9e4
k-alex-leon <kelemon824@gmail.com>
2024-07-20 12:35:01 -0500

    style: home page update

---
+++ frontend/src/data/tips.ts
@@ -0,0 +1,26 @@
+export const tipsList = [
+    {
+      title: "Vacunación Antirábica",
+      description: `La vacuna antirrábica debe aplicarse a perros y gatos 
+      con buen estado de salud a partir de los 3 (TRES) meses de edad y 
+      revacunarse anualmente, o de acuerdo a las undicaciones del veterinario...`,
+      link: "#",
+      image: "./vaccines.jpg",
+    },
+    {
+      title: "Fuente de agua limpia",
+      description: `El consumo de agua es uno de los puntos más importantes para 
+      mantener la salud y el bienestar de nuestras mascotas. 
+      Los perros deben consumir aproximadamente...`,
+      link: "#",
+      image: "./clean-water.jpg",
+    },
+    {
+      title: "Paseos con cuidadores",
+      description: `Cuando te prepares para recibir a un cuidador, 
+      piensa en los detalles de la rutina de tu mascota que para ti son naturales, 
+      pero que el nuevo cuidador necesitará conocer. Estos son...`,
+      link: "#",
+      image: "./walk.jpg",
+    },
+  ];
