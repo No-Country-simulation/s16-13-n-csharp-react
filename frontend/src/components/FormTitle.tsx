2015ef5e1b24b6e8706f7a8e5857488aed81ba55
k-alex-leon <kelemon824@gmail.com>
2024-07-29 17:12:35 -0500

    style: add margin bottom

--- frontend/src/components/FormTitle.tsx
+++ frontend/src/components/FormTitle.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 }
 
 export const FormTitle: React.FC<TitleProps> = ({title = '', style}) => (
-    <h4 className={`text-primary-darker font-Poppins w-full text-center font-bold lg:text-2xl md:text-xl ${style}`}>
+    <h4 className={`text-primary-darker font-Poppins w-full mb-6 text-center font-bold lg:text-2xl md:text-xl ${style}`}>
       {title}
     </h4>
   );
