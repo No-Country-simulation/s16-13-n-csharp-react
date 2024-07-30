0dffb82775c89917cbc1721a3fb8266552f7e933
k-alex-leon <kelemon824@gmail.com>
2024-07-29 19:35:41 -0500

    refactor: toolcards created

--- frontend/src/components/CarnetButtons.tsx
+++ frontend/src/components/CarnetButtons.tsx
@@ -1,48 +1,40 @@
 import { Link } from "react-router-dom";
 
+interface ToolCardProps {
+  title: string;
+  link?: string;
+  content?: any;
+}
+
+const ToolCard = ({ title, link = "#", content }: ToolCardProps) => {
+  return (
+    <div className=" w-[296px] h-[213px px-[15px] flex flex-col gap-[42px] items-center justify-center pt-[52px] pb-[34px] custom-box-shadow rounded-[20px] bg-[#F9FCFA]">
+      <h2 className=" font-Poppins w-full text-center text-[32px] leading-[48px] text-[#37636A] font-medium">
+        {title}
+      </h2>
+      {!content ? (
+        <Link
+          className="py-3 text-2xl font-bold leading-9 font-Poppins bg-[#4A848E] text-white rounded-[10px] w-[199px] flex justify-center"
+          to={link}
+        >
+          Agregar
+        </Link>
+      ) : (
+        content
+      )}
+    </div>
+  );
+};
+
 export default function CarnetButtons() {
   return (
-    <section className="flex flex-col gap-[132px]">
-      <div className="flex gap-[110px]">
-        <div className=" w-[296px] h-[213px px-[15px] flex flex-col gap-[42px] items-center justify-center pt-[52px] pb-[34px] custom-box-shadow rounded-[20px] bg-[#F9FCFA]">
-          <h2 className=" font-Poppins text-[32px] leading-[48px] text-[#37636A] font-medium">
-            Historial Médico
-          </h2>
-          <Link
-            className="py-3 text-2xl font-bold leading-9 font-Poppins bg-[#4A848E] text-white rounded-[10px] w-[199px] flex justify-center"
-            to="#"
-          >
-            Agregar
-          </Link>
-        </div>
-        <div className=" w-[296px] h-[213px px-[15px] flex flex-col gap-[42px] items-center justify-center pt-[52px] pb-[34px] custom-box-shadow rounded-[20px] bg-[#F9FCFA]">
-          <h2 className=" font-Poppins text-[32px] leading-[48px] text-[#37636A] font-medium text-center">
-            Registro de Vacunas
-          </h2>
-          <Link
-            className="py-3 text-2xl font-bold leading-9 font-Poppins bg-[#4A848E] text-white rounded-[10px] w-[199px] flex justify-center"
-            to="#"
-          >
-            Agregar
-          </Link>
-        </div>
-      </div>
-      <div className="flex gap-[110px]">
-        <div className=" w-[296px] h-[213px px-[15px] flex flex-col gap-[42px] items-center justify-center pt-[52px] pb-[34px] custom-box-shadow rounded-[20px] bg-[#F9FCFA]">
-          <h2 className=" font-Poppins text-[32px] leading-[48px] text-[#37636A] font-medium">
-            Recordatorios
-          </h2>
-          <Link
-            className="py-3 text-2xl font-bold leading-9 font-Poppins bg-[#4A848E] text-white rounded-[10px] w-[199px] flex justify-center"
-            to="#"
-          >
-            Agregar
-          </Link>
-        </div>
-        <div className=" w-[296px] h-[213px px-[15px] flex flex-col gap-[42px] items-center justify-center pt-[52px] pb-[34px] custom-box-shadow rounded-[20px] bg-[#F9FCFA]">
-          <h2 className=" font-Poppins text-[32px] leading-[48px] text-[#37636A] font-medium">
-            Notificaciones
-          </h2>
+    <section className="grid grid-cols-2 grid-rows-2 gap-20 p-10">
+      <ToolCard title="Historial Médico" link="../modal/medical-history" />
+      <ToolCard title="Registro de Vacunas" link="../modal/vaccines" />
+      <ToolCard title="Recordatorios" link="../modal/reminders" />
+      <ToolCard
+        title="Notificaciones"
+        content={
           <div className="flex items-center gap-2">
             <input
               className="rounded-[20px]"
@@ -56,8 +48,8 @@ export default function CarnetButtons() {
               Usted aún no tiene ninguna notificación
             </label>
           </div>
-        </div>
-      </div>
+        }
+      />
     </section>
   );
 }
