7e03a174f891396073b62238dffb78eac0eae4e4
k-alex-leon <kelemon824@gmail.com>
2024-08-01 14:11:57 -0500

    feat: get id from url params

--- frontend/src/pages/QR/Qr.tsx
+++ frontend/src/pages/QR/Qr.tsx
@@ -1,25 +1,58 @@
-import { useParams } from "react-router";
 import PetInfo from "./Components/PetInfo";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { notify_error } from "../../utils/notifications";
+import axios from "axios";
+import { BASE_URL } from "../../utils/const";
+
+const initialPetData = {
+  id : 0,
+  mascotName: "",
+  species: "Perro",
+  breed: "",
+  sex: "Hembra",
+  dateOfBirth: "2024-08-22T00:00:00Z",
+  mascotPhoto:
+    "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS",
+  ownerFullName: "",
+  ownerPhoneNumber: "",
+};
 
 export const Qr = () => {
-  const { id } = useParams();
+  const [id, setId] = useState<string | null>(null);
+  const [petData, setPetData] = useState(initialPetData);
 
-  const petData = {
-    name: "Buddy",
-    species: "Dog",
-    gender: "Male",
-    breed: "Golden Retriever",
-    birthDate: "2020/01/01",
-    owner: "John Doe",
-    contactNumber: "123-456-7890",
-    imageUrl:
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS",
+  const getPetData = async () => {
+    if (!id) {
+      notify_error("No encontramos el id!");
+      return;
+    }
+    try {
+      const response = await axios.get(BASE_URL + `QR/RecuperarMascota/${id}`);
+
+      return response.data;
+    } catch (error) {
+      notify_error("🐕 Ups! No encontramos información...🐈");
+    }
   };
 
   useEffect(() => {
-    console.log('id from params', id)
-  }, [id])
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const idParam = urlSearchParams.get("id");
+    getPetData().then((data) => setPetData({ ...data, id }));
+    setId(idParam);
+  }, []);
+
+  // const petData = {
+  //   name: "Buddy",
+  //   species: "Dog",
+  //   gender: "Male",
+  //   breed: "Golden Retriever",
+  //   birthDate: "2020/01/01",
+  //   owner: "John Doe",
+  //   contactNumber: "123-456-7890",
+  //   imageUrl:
+  //     "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS",
+  // };
 
   return (
     <>
