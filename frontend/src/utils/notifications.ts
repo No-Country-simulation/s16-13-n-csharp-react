909640c5db020e433cdbcad84a708319a4ef5a48
k-alex-leon <kelemon824@gmail.com>
2024-07-31 23:03:02 -0500

    feat: toastify notifications

---
+++ frontend/src/utils/notifications.ts
@@ -0,0 +1,29 @@
+import { Bounce, toast } from "react-toastify";
+
+export const notify_success = (msg = "") => {
+  return toast.success(msg, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  });
+};
+
+export const notify_error = (msg = "") => {
+  return toast.error(msg, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  });
+};
